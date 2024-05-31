26d8119dfd4840b5200c4b0d8b64c981cd4734cd
HINA IRFAN <160302309+hinairfan4@users.noreply.github.com>
2024-05-31 21:09:06 +0500

    add files

---
+++ app.js
@@ -0,0 +1,31 @@
+// let firstName="HINA IRFAN";
+// alert("firstName");
+// //DataTYPES//
+
+// String1
+// Number2
+// Boolean3
+// let myluckNumber=8;//"8"//
+// // exaple,43,57,4,765//
+//number adeshen//
+// let data="5";
+// let data2=6;
+// let data3=data + data2; //string concatenation//
+// console.log(dacstta3)
+// let data=("Hello")
+// let data2=data.includes("H");
+// close.log(data2);
+// let userName=prompt("what is your name?");
+// alert("welcom to my website" +userName);
+// let myluckNumber=prompt=7;
+// console.log(43,37,0,34,4.765);
+// // let randomNumber =MathrandomNumber
+//  randomNUMBER=randomNUMBER*10;
+//  console.log(randomNumber);
+//  let randomNUMBER=Math.round(randomNumber);
+//  console.log(randomNumber);
+let num= 5**3;
+console.log(num);
+
+
+
